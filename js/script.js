3369be090bef5d8d569193252733faa684532cf9
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2022-03-24 16:02:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,11 @@ const choices = new Choices(selectElem, {
 });
 
 const getData = async (url) => {
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      'X-Api-Key': API_KEY,
+    }
+  });
   const data = await response.json();
   return data;
 };
@@ -52,7 +56,7 @@ const renderCard = (data) => {
 };
 
 const loadNews = async () => {
-  const data = await getData(`https://newsapi.org/v2/top-headlines?country=ru&language=ru&apiKey=${API_KEY}`);
+  const data = await getData('https://newsapi.org/v2/top-headlines?country=ru&language=ru');
   renderCard(data.articles);
 };
 
